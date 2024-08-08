964cff8d64379ac52f36d68a7dbeeb01ab633564
Jackson Ribeiro <jacksonribeiro@bemol.com.br>
2024-08-07 21:54:49 -0400

    Inserção da classes de Menu para criar e listar dados da conta.

---
+++ Menu/MenuCreateAccount.cs
@@ -0,0 +1,54 @@
+﻿using FlorestBank.Model;
+
+namespace FlorestBank.Menu;
+
+internal class MenuCreateAccount
+{
+    public void CriarContaCorrente(List<Conta> contas)
+    {
+        Console.Clear();
+        Console.WriteLine($"---- Criar Conta Corrente ----");
+
+        Console.Write("Nome do titular: ");
+        string nomeTitulo = Console.ReadLine()!;
+
+        Console.Write("Número de conta: ");
+        int numeroConta = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Saldo: ");
+        int saldo = int.Parse(Console.ReadLine()!);
+
+        TipoConta contaCorrente = new TipoConta();
+
+        Conta newConta = new Conta(nomeTitulo, numeroConta, saldo, contaCorrente);
+        contas.Add(newConta);
+
+        Console.WriteLine("\nAperte na tecla 'Enter' para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+    public void CriarContaPoupanca(List<Conta> contas)
+    {
+        Console.Clear();
+        Console.WriteLine($"---- Criar Conta Poupança ----");
+
+        Console.Write("Nome do titular: ");
+        string nomeTitulo = Console.ReadLine()!;
+
+        Console.Write("Número de conta: ");
+        int numeroConta = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Saldo: ");
+        int saldo = int.Parse(Console.ReadLine()!);
+
+        TipoConta contaPoupanca = new TipoConta();
+
+        Conta newConta = new Conta(nomeTitulo, numeroConta, saldo, contaPoupanca);
+        contas.Add(newConta);
+
+        Console.WriteLine("\nAperte na tecla 'Enter' para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
---
+++ Menu/MenuListAccount.cs
@@ -0,0 +1,66 @@
+﻿using FlorestBank.Model;
+
+namespace FlorestBank.Menu;
+
+internal class MenuListAccount
+{
+    public void OrdenarContaPorNome(List<Conta> contas)
+    {
+        Console.Clear();
+        if (contas.Count != 0)
+        {
+            Console.WriteLine("---- Contas registradas no sistema ----");
+
+            var ordenarPorNome = contas.OrderBy(a => a.Titular);
+
+            foreach (var conta in ordenarPorNome)
+            {
+                Console.WriteLine($"\nNome do titular: {conta.Titular}");
+                Console.WriteLine($"Número da conta: {conta.Numero}");
+                Console.WriteLine($"Saldo: {conta.Saldo}");
+
+            }
+            Console.WriteLine("\nAperte na tecla 'Enter' para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine("Nenhuma conta registrada no sistema.");
+            Console.WriteLine("\nAperte na tecla 'Enter' para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+
+    public void OrdenarContaPorNumero(List<Conta> contas)
+    {
+        Console.Clear();
+        if (contas.Count != 0)
+        {
+            Console.WriteLine("---- Contas registradas no sistema ----");
+
+            var ordenarPorNumero = contas.OrderBy(a => a.Numero);
+            foreach (var conta in ordenarPorNumero)
+            {
+                Console.WriteLine($"\nConta: {conta.Tipo.TipoContaUsuario}");
+                Console.WriteLine($"\nNome do titular: {conta.Titular}");
+                Console.WriteLine($"Número da conta: {conta.Numero}");
+                Console.WriteLine($"Saldo: {conta.Saldo}");
+
+            }
+            Console.WriteLine("\nAperte na tecla 'Enter' para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine("Nenhuma conta registrada no sistema.");
+            Console.WriteLine("\nAperte na tecla 'Enter' para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+
+}
